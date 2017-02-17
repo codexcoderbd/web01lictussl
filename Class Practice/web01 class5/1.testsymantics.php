2fe5ee112882be0bfe07c01eb8994267a8e8efaf
codexcoderbd <codexcoderbd@gmail.com>
2017-02-17 11:32:22 +0600

    test

---
+++ Class Practice/web01 class5/1.testsymantics.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HTML Symantics</title>
+</head>
+
+
+
+<body>
+
+<h3 style="color:blue;">Nav Bar</h3>
+
+<nav>
+    <a href="#">Home</a> |
+    <a href="#">About Us</a> |
+    <a href="#">Contact Us</a> |
+</nav>
+
+<div>
+    <h3 style="color:blue;">
+        Footer Element
+    </h3>
+    <footer>
+        <p>Posted by: Ibrahim Arafat</p>
+        <p>Contact Information: <a href="arafat0112358@gmail.com">arafat0112358@gmail.com</a>. </p>
+        <p>Contact Information: <a href="mailto:arafat0112358@gmail.com">arafat0112358@gmail.com</a>. </p>
+    </footer>
+</div>
+
+<div>
+    <h3 style="color:blue;">Aside Element</h3>
+    <aside>
+        <h4>
+            Lorem Center
+        </h4>
+        <p>Aliquam sed dapibus ante. Praesent enim purus, euismod non sagittis vel, faucibus ac nisl. Aenean id libero vestibulum, pretium lorem ut, molestie magna. Proin maximus erat at consequat scelerisque. Quisque at erat at diam cursus porta. Pellentesque et diam facilisis, varius felis non, porta orci. Morbi odio nisl, convallis eu luctus eget, varius at libero.</p>
+    </aside>
+</div>
+
+<div>
+    <h3 style="color:blue;">figure and figure caption Element</h3>
+    <figure>
+        <img src="Desert.jpg" alt="Desert" width="304" height="288">
+        <figcaption>
+            Fig1. - The Desert, Nowhere.
+        </figcaption>
+    </figure>
+</div>
+
+<div>
+    <h3 style="color:blue;">Section Element</h3>
+
+<section>
+    <h1>US SOFT</h1>
+    <p>LICT is a project of Bangladesh Computer Council (BCC)</p>
+</section>
+</div>
+
+<div>
+    <h3 style="color:blue;">Article Element</h3>
+    <article>
+        <h1>What is Lorem Ipsum ?</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis lacus nec aliquam venenatis. Fusce sollicitudin metus in auctor porttitor. Duis ornare lorem at risus vehicula, sit amet dapibus libero scelerisque. Suspendisse in erat dolor. Nam vitae felis ut erat viverra viverra ut ut ipsum. Donec suscipit ultrices massa in pretium. Proin sodales interdum ante, at sagittis lacus tincidunt sit amet. Duis eleifend posuere ante ac gravida. Aliquam erat volutpat. Nunc tincidunt sem porta metus molestie, vitae bibendum dui placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce gravida libero sed lorem congue scelerisque. Nulla laoreet eu eros ac hendrerit.
+        </p>
+    </article>
+</div>
+
+<div>
+    <h3 style="color:blue;">Header lement</h3>
+    <article>
+        <header>
+            <h1>What Does Lorem Ipsum ?</h1>
+            <p>Lorems mission:</p>
+        </header>
+        <p>Fusce facilisis consequat orci, eu consequat erat ultrices a. Etiam tempus vitae magna eu mattis. Nulla quis mi nec mi rutrum tincidunt. Vestibulum lobortis dapibus ipsum, vel tempor risus dapibus quis. Etiam blandit felis eget eros auctor molestie. Aliquam turpis sem, placerat lacinia faucibus vitae, auctor sit amet erat. Nunc luctus massa vel est consequat, sit amet volutpat ligula egestas.</p>
+    </article>
+</div>
+
+</body>
+</html>
---
+++ Class Practice/web01 class5/2. css practice.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <style>
+
+/*        *{
+            color: red;  !*Universal Selector*!
+        }*/
+        ul li{
+            color:green;
+        }
+
+/*        h1{
+            color:maroon;
+            font-size: 30px;
+
+        }
+
+        h3{
+            color:#00bf00;
+
+        } */
+
+        p{
+            text-align: center;
+            color:darkblue;
+
+        }
+
+        #para1{
+            text-allign:center;
+            color: maroon;
+        }
+
+    </style>
+
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+<header>
+    <h1>What Does Lorem Ipsum ?</h1>
+    <p>Lorems mission:</p>
+</header>
+</div>
+
+<div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a> </li>
+            <li><a href="#">About Us</a> </li>
+            <li><a href="#">Contact Us</a> </li>
+        </ul>
+    </nav>
+</div>
+
+<div>
+    <body>
+    <p id="para1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+    </p>
+    <p>
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </body>
+</div>
+
+<div>
+    <h3 >Article Element</h3>
+<article>
+    <h1>What is Lorem Ipsum ?</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis lacus nec aliquam venenatis. Fusce sollicitudin metus in auctor porttitor. Duis ornare lorem at risus vehicula, sit amet dapibus libero scelerisque. Suspendisse in erat dolor. Nam vitae felis ut erat viverra viverra ut ut ipsum. Donec suscipit ultrices massa in pretium. Proin sodales interdum ante, at sagittis lacus tincidunt sit amet. Duis eleifend posuere ante ac gravida. Aliquam erat volutpat. Nunc tincidunt sem porta metus molestie, vitae bibendum dui placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce gravida libero sed lorem congue scelerisque. Nulla laoreet eu eros ac hendrerit.
+    </p>
+</article>
+</div>
+
+<div>
+
+    <footer>
+        <p>Posted by: Ibrahim Arafat</p>
+        <p>Contact Information: <a href="arafat0112358@gmail.com">arafat0112358@gmail.com</a>. </p>
+        <p>Contact Information: <a href="mailto:arafat0112358@gmail.com">arafat0112358@gmail.com</a>. </p>
+    </footer>
+</div>
+
+</body>
+</html>
